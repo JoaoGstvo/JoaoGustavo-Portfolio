12cc7cfa7ecab0ef2c9eb67ee943faf254148a3c
João Gustavo <joaogustavocarmomoraes@gmail.com>
2024-11-22 19:17:26 -0300

    Testes - CSS - Página Inicial

--- meu-site/src/pages/home-portugues/index.js
+++ meu-site/src/pages/home-portugues/index.js
@@ -3,49 +3,49 @@ import './index.css'
 function PaginaInicial() {
     return(
         <main>
-            <section className="section-linguagem">
-                <div>
-                    <h1>
+            <section className="section-linguagem" id="id-section">
+                <div className="div-linguagem">
+                    <h1 className="textos">
                         English
                     </h1>
                 </div>
             </section>
 
-            <section className="itens-superiores">
+            <section className="itens-superiores" id="id-section">
                 <div className="div-1" id="id-divs">
-                    <h1>
+                    <h1 className="textos">
                         Objetivos
                     </h1>
                 </div>
                 {/* --------------------------------------------- */}
                 <div className="div-2" id="id-divs">
-                    <h1>
+                    <h1 className="textos">
                         Tecnologias
                     </h1>
                 </div>
                 {/* --------------------------------------------- */}                
                 <div className="div-3" id="id-divs">
-                    <h1>
+                    <h1 className="textos">
                         Projetos
                     </h1>
                 </div>
             </section>
 
-            <section className="itens-meio">
+            <section className="itens-meio" id="id-section">
                 <div className="div-4" id="id-divs">
-                    <h1>
+                    <h1 className="textos">
                         Idiomas
                     </h1>
                 </div>
                 {/* --------------------------------------------- */}
                 <div className="div-5" id="id-divs">
-                    <h1>
+                    <h1 className="textos">
                         João Gustavo
                     </h1>
                 </div>
                 {/* --------------------------------------------- */}
                 <div className="div-6" id="id-divs">
-                    <h1>
+                    <h1 className="textos">
                         Contatos
                     </h1>
                 </div>
@@ -53,19 +53,19 @@ function PaginaInicial() {
 
             <section className="itens-inferiores">
                 <div className="div-7" id="id-divs">
-                    <h1>
+                    <h1 className="textos">
                         Formações
                     </h1>
                 </div>
                 {/* --------------------------------------------- */}
                 <div className="div-8" id="id-divs">
-                    <h1>
+                    <h1 className="textos">
                         Informações
                     </h1>
                 </div>
                 {/* --------------------------------------------- */}
                 <div className="div-9" id="id-divs">
-                    <h1>
+                    <h1 className="textos">
                         Informações
                     </h1>
                 </div>
