f27f7703de799349248435f6b6b6cd69ab4ab7bf
João Gustavo <joaogustavocarmomoraes@gmail.com>
2024-11-25 21:52:26 -0300

    Ajustes

--- meu-site/src/pages/home-portugues/index.js
+++ meu-site/src/pages/home-portugues/index.js
@@ -12,19 +12,19 @@ function PaginaInicial() {
             </section>
 
             <section className="itens-superiores" id="id-section">
-                <div className="div-1" id="id-divs">
+                <div className="id-divs" id="div-1">
                     <h1 className="textos">
                         Objetivos
                     </h1>
                 </div>
                 {/* --------------------------------------------- */}
-                <div className="div-2" id="id-divs">
+                <div className="id-divs" id="div-2">
                     <h1 className="textos">
                         Tecnologias
                     </h1>
                 </div>
                 {/* --------------------------------------------- */}                
-                <div className="div-3" id="id-divs">
+                <div className="id-divs" id="div-3">
                     <h1 className="textos">
                         Projetos
                     </h1>
@@ -32,41 +32,32 @@ function PaginaInicial() {
             </section>
 
             <section className="itens-meio" id="id-section">
-                <div className="div-4" id="id-divs">
+
+                <div className="id-divs" id="div-4">
+                    <h1 className="textos">
+                        João Gustavo
+                    </h1>
+                </div>
+              
+            </section>
+
+            <section className="itens-inferiores">
+                <div className="id-divs" id="div-5">
+                    <h1 className="textos">
+                        Formações
+                    </h1>
+                </div>
+
+                {/* --------------------------------------------- */}
+                <div className="id-divs" id="div-6">
                     <h1 className="textos">
                         Idiomas
                     </h1>
                 </div>
                 {/* --------------------------------------------- */}
-                <div className="div-5" id="id-divs">
-                    <h1 className="textos">
-                        João Gustavo
-                    </h1>
-                </div>
-                {/* --------------------------------------------- */}
-                <div className="div-6" id="id-divs">
+                <div className="id-divs" id="div-7">
                     <h1 className="textos">
                         Contatos
-                    </h1>
-                </div>
-            </section>
-
-            <section className="itens-inferiores">
-                <div className="div-7" id="id-divs">
-                    <h1 className="textos">
-                        Formações
-                    </h1>
-                </div>
-                {/* --------------------------------------------- */}
-                <div className="div-8" id="id-divs">
-                    <h1 className="textos">
-                        Informações
-                    </h1>
-                </div>
-                {/* --------------------------------------------- */}
-                <div className="div-9" id="id-divs">
-                    <h1 className="textos">
-                        Informações
                     </h1>
                 </div>
             </section>
